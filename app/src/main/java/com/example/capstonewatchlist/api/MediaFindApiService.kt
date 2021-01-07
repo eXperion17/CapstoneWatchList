ff5808e883a85cf4a7b2dad2e15a7253acc9713d
Marc Gomersbach <marc.gomersbach@hva.nl>
2021-01-07 16:53:30 +0100

    Moved the API Key to its own property file & adjusted the .gitignore so it doesn't get shared

--- app/src/main/java/com/example/capstonewatchlist/api/MediaFindApiService.kt
+++ app/src/main/java/com/example/capstonewatchlist/api/MediaFindApiService.kt
@@ -1,15 +1,18 @@
 package com.example.capstonewatchlist.api
 
+import com.example.capstonewatchlist.BuildConfig
 import com.example.capstonewatchlist.model.MediaSearchResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+const val API_KEY: String = BuildConfig.API_KEY
+
 interface MediaFindApiService {
-    @GET("search/movie?api_key=7d5c4def970e7f89c0b441a107534faf" +
+    @GET("search/movie?api_key=$API_KEY" +
             "&language=en-US&sort_by=popularity.desc&page=1&include_adult=false")
     suspend fun getMoviesWithTitle(@Query("query") title:String): MediaSearchResult
 
-    @GET("search/tv?api_key=7d5c4def970e7f89c0b441a107534faf" +
+    @GET("search/tv?api_key=$API_KEY" +
             "&language=en-US&sort_by=popularity.desc&page=1&include_adult=false")
     suspend fun getShowsWithTitle(@Query("query") title:String): MediaSearchResult
 }
